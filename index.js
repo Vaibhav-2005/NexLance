d59fbb0731ed11aede1aa01ed93aae7013bcbb3e
Vaibhav-2005 <codevaibhav2005@gmail.com>
2025-04-11 05:51:32 +0530

    commit

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://nexlance.netlify.app',
-    credentials:true
+    origin: "https://nexlance.netlify.app",
+    credentials: true
 }
 
 app.use(cors(corsOptions));
